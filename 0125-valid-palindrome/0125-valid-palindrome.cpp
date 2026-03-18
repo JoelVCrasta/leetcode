19d4010e38f858e68019e7b4158bfd255ce62c1a
Joel Crasta <22b03.joelc@sjec.ac.in>
2026-03-18 11:00:24 +0530

    Time: 1 ms (44.91%), Space: 9.9 MB (53.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string pal = "";
-
-        for (auto c : s) {
-            if(isalnum(c)) {
-                pal += tolower(c);
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (!isalnum(s[left])) {
+                left++;
+            } else if (!isalnum(s[right])) {
+                right--;
+            } else {
+                if (tolower(s[left]) != tolower(s[right])) return false;
+                left++;
+                right--;
             }
         }
-
-        int i = 0, j = pal.length() - 1;
-        while (i < j) {
-            if (pal[i] != pal[j]) return false;
-            i++; j--;
-        }
-
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string pal = "";
+
+//         for (auto c : s) {
+//             if(isalnum(c)) {
+//                 pal += tolower(c);
+//             }
+//         }
+
+//         int i = 0, j = pal.length() - 1;
+//         while (i < j) {
+//             if (pal[i] != pal[j]) return false;
+//             i++; j--;
+//         }
+
+//         return true;
+//     }
+// };
