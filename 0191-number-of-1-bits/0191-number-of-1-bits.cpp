3f8daf52efa5231383b0312727d5a35b83fa3eb4
Joel Crasta <22b03.joelc@sjec.ac.in>
2026-03-20 11:31:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.14%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int mask = 1;
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if (n & mask) count++;
+            mask <<= 1;
+        }
+
+        return count;
+    }
+};
