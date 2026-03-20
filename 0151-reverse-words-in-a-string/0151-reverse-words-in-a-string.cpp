c0324f2ef433c5375385e2499eea2b8379d110aa
Joel Crasta <22b03.joelc@sjec.ac.in>
2026-03-20 09:32:26 +0530

    Time: 0 ms (100%), Space: 9.9 MB (87.31%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result("");
-        string sub("");
-        vector <string> store;
-        int j = -1, len = 0;
-        int size = s.length();
+        int n = s.length();
+        int write_idx = 0;
 
-        for (int i = 0 ; i < size ; i++){
-            if (s[i] == ' ') {    
-                if (j != -1){
-                    sub = s.substr(j, len);
-                    store.push_back(sub);
-                    j = -1;
+        reverse(s.begin(), s.end());
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ' ') {
+                if (write_idx != 0) s[write_idx++] = ' ';
+
+                int start = write_idx;
+                while(i < n && s[i] != ' ') {
+                    s[write_idx++] = s[i++];
                 }
-                continue;
-            }
-            if (j == -1)  j = i; 
-            if (j != -1 && s[i] != ' ') len = i - j + 1;
-            if(j != -1 && i == size - 1 ) {
-                sub = s.substr(j, len);
-                store.push_back(sub);
+
+                reverse(s.begin() + start, s.begin() + write_idx);
             }
         }
 
-        for (int k = store.size() - 1 ; k >= 0 ; k--){
-            if (k != 0) result += store[k] + " ";
-            else result += store[k];
-        }
+        s.resize(write_idx);
 
-        return result;
+        return s;
     }
 };
