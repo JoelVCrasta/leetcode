70161c0285736c9f5fa25041989ef057cb4554e7
Joel Crasta <22b03.joelc@sjec.ac.in>
2026-03-23 10:38:35 +0530

    Time: 8 ms (96.17%), Space: 24.9 MB (86.71%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+        vector<vector<string>> result;
+
+        for (const string& str : strs) {
+            string temp = str;
+            sort(temp.begin(), temp.end());
+
+            map[temp].push_back(str);
+        }
+
+        for (const auto& m : map) {
+            result.push_back(m.second);
+        }
+
+        return result;
+    }
+};
