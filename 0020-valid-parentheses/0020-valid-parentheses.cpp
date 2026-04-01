da2619f2e7064655e67b1ec31dbbb4f20126fd3d
Joel Crasta <22b03.joelc@sjec.ac.in>
2026-04-01 09:33:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.73%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        unordered_map<char, char> map = { {')', '('}, {'}', '{'}, {']', '['} };
+
+        for (char c : s) {
+            if(map.count(c)) {
+                if (stk.empty() || map[c] != stk.top()) {
+                    return false;
+                }
+
+                stk.pop();
+            } else {
+                stk.push(c);
+            }
+        }
+
+        return stk.empty();
+    }
+};
